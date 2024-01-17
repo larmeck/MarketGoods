4f733ded1eae18fabf8ff4411c8991e9474dc320
LarmeckCompany <larmeckoduor@gmail.com>
2024-01-17 14:55:25 +0300

    compares the prices of different items

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+function product1(){
+   
+    if(document.getElementById("mybutton").onclick){
+        document.getElementById("mylabel").innerHTML ="Woolmat: "+ 120;
+        document.getElementById("mylabel2").innerHTML ="Naivas: "+ 125;
+    }
+    else{
+        document.getElementById("mylabel").innerHTML ="Null";
+        document.getElementById("mylabel2").innerHTML = "Null";
+    }
+}
+
+function product2(){
+
+   if(document.getElementById("mybutton1").onclick){
+        document.getElementById("mylabel3").innerHTML ="Woolmat: "+ 204;
+        document.getElementById("mylabel4").innerHTML ="Naivas: "+ 207;
+    }
+    else{
+        document.getElementById("mylabe3").innerHTML ="Null";
+        document.getElementById("mylabel4").innerHTML = "Null";
+    }
+}
+
+function product3(){
+     if(document.getElementById("mybutton2").onclick){
+        document.getElementById("mylabel5").innerHTML ="Woolmat: "+ 320;
+        document.getElementById("mylabel6").innerHTML ="Naivas: "+ 425;
+    }
+    else{
+        document.getElementById("mylabel5").innerHTML ="Null";
+        document.getElementById("mylabel6").innerHTML = "Null";
+    }
+}
+
+function product4(){
+    if(document.getElementById("mybutton3").onclick){
+        document.getElementById("mylabel7").innerHTML ="Woolmat: "+ 512;
+        document.getElementById("mylabel8").innerHTML ="Naivas: "+ 470;
+    }
+    else{
+        document.getElementById("mylabel7").innerHTML ="Null";
+        document.getElementById("mylabel8").innerHTML = "Null";
+    }
+}
+
+
+
