f7557a6b7a528c6fae2be187ec897a10804addf9
LarmeckCompany <larmeckoduor@gmail.com>
2024-01-17 20:15:38 +0300

    added more items with their pricing, and logos of market prices coming soon

--- index.js
+++ index.js
@@ -1,50 +1,93 @@
 
 
-function product1(){
-   
-    if(document.getElementById("mybutton").onclick){
-        document.getElementById("mylabel").innerHTML ="Woolmat: "+ 120;
-        document.getElementById("mylabel2").innerHTML ="Naivas: "+ 125;
+    function product1(){
+    
+        if(document.getElementById("mybutton").onclick){
+            document.getElementById("mylabel").innerHTML ="Woolmat: "+ 120;
+            document.getElementById("mylabel2").innerHTML ="Naivas: "+ 125;
+        }
+        else{
+            document.getElementById("mylabel").innerHTML ="Null";
+            document.getElementById("mylabel2").innerHTML = "Null";
+        }
     }
-    else{
-        document.getElementById("mylabel").innerHTML ="Null";
-        document.getElementById("mylabel2").innerHTML = "Null";
-    }
-}
 
-function product2(){
+    function product2(){
 
-   if(document.getElementById("mybutton1").onclick){
-        document.getElementById("mylabel3").innerHTML ="Woolmat: "+ 204;
-        document.getElementById("mylabel4").innerHTML ="Naivas: "+ 207;
+    if(document.getElementById("mybutton1").onclick){
+            document.getElementById("mylabel3").innerHTML ="Woolmat: "+ 204;
+            document.getElementById("mylabel4").innerHTML ="Naivas: "+ 207;
+        }
+        else{
+            document.getElementById("mylabe3").innerHTML ="Null";
+            document.getElementById("mylabel4").innerHTML = "Null";
+        }
     }
-    else{
-        document.getElementById("mylabe3").innerHTML ="Null";
-        document.getElementById("mylabel4").innerHTML = "Null";
-    }
-}
 
-function product3(){
-     if(document.getElementById("mybutton2").onclick){
-        document.getElementById("mylabel5").innerHTML ="Woolmat: "+ 320;
-        document.getElementById("mylabel6").innerHTML ="Naivas: "+ 425;
+    function product3(){
+        if(document.getElementById("mybutton2").onclick){
+            document.getElementById("mylabel5").innerHTML ="Woolmat: "+ 320;
+            document.getElementById("mylabel6").innerHTML ="Naivas: "+ 425;
+        }
+        else{
+            document.getElementById("mylabel5").innerHTML ="Null";
+            document.getElementById("mylabel6").innerHTML = "Null";
+        }
     }
-    else{
-        document.getElementById("mylabel5").innerHTML ="Null";
-        document.getElementById("mylabel6").innerHTML = "Null";
+
+    function product4(){
+        if(document.getElementById("mybutton3").onclick){
+            document.getElementById("mylabel7").innerHTML ="Woolmat: "+ 512;
+            document.getElementById("mylabel8").innerHTML ="Naivas: "+ 470;
+        }
+        else{
+            document.getElementById("mylabel7").innerHTML ="Null";
+            document.getElementById("mylabel8").innerHTML = "Null";
+        }
     }
-}
 
-function product4(){
-    if(document.getElementById("mybutton3").onclick){
-        document.getElementById("mylabel7").innerHTML ="Woolmat: "+ 512;
-        document.getElementById("mylabel8").innerHTML ="Naivas: "+ 470;
+
+
+    function product5(){
+        if(document.getElementById("mybutton4").onclick){
+            document.getElementById("mylabel9").innerHTML ="Woolmat: "+ 500;
+            document.getElementById("mylabel10").innerHTML ="Naivas: "+ 430;
+        }
+        else{
+            document.getElementById("mylabel9").innerHTML ="Null";
+            document.getElementById("mylabel10").innerHTML = "Null";
+        }
     }
-    else{
-        document.getElementById("mylabel7").innerHTML ="Null";
-        document.getElementById("mylabel8").innerHTML = "Null";
+
+    function product6(){
+        if(document.getElementById("mybutton5").onclick){
+            document.getElementById("mylabel11").innerHTML ="Woolmat: "+ 199;
+            document.getElementById("mylabel12").innerHTML ="Naivas: "+ 205;
+        }
+        else{
+            document.getElementById("mylabel11").innerHTML ="Null";
+            document.getElementById("mylabel12").innerHTML = "Null";
+        }
     }
-}
 
+    function product7(){
+        if(document.getElementById("mybutton6").onclick){
+            document.getElementById("mylabel13").innerHTML ="Woolmat: "+ 562;
+            document.getElementById("mylabel14").innerHTML ="Naivas: "+ 540;
+        }
+        else{
+            document.getElementById("mylabel3").innerHTML ="Null";
+            document.getElementById("mylabel4").innerHTML = "Null";
+        }
+    }
 
-
+    function product8(){
+        if(document.getElementById("mybutton7").onclick){
+            document.getElementById("mylabel15").innerHTML ="Woolmat: "+ 732;
+            document.getElementById("mylabel16").innerHTML ="Naivas: "+ 800;
+        }
+        else{
+            document.getElementById("mylabel15").innerHTML ="Null";
+            document.getElementById("mylabel16").innerHTML = "Null";
+        }
+    }
